67f9567efc09948772a899e0a1bfe9cd3232cabf
Artemiy Sartakov <artemiy.sartakov@jetbrains.com>
2021-05-25 15:34:39 +0700

    final

---
+++ final/src/Nineteen.java
@@ -0,0 +1,14 @@
+public class Nineteen {
+    private interface MyI {
+        String doSmth(String s);
+
+        default int getLenAfter(String s) {
+            return doSmth(s).length();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyI myI = String::trim;
+        System.out.println(myI.getLenAfter(" foo "));
+    }
+}
